5380daf943637a872c1f9a9914aaefce28f0a24c
bakastrumpf <baka.strumpf@gmail.com>
2021-10-31 22:45:16 +0100

    end 20211031

--- src/BookList.js
+++ src/BookList.js
@@ -15,6 +15,7 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import { ListRounded } from "@mui/icons-material";
 
 const BookList = ({ list, onDelete }) => {
     return <div className="kartice">
